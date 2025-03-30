8c744e73e424cc17d6f27dc78bef97d353059b8e
Austin Spencer <abspencer2097@gmail.com>
2025-03-30 11:21:28 -0400

    CHORE: Upgrade client and server dependencies (#19)
    * CHORE: Update Dependencies

--- entrypoint-server.sh
+++ entrypoint-server.sh
@@ -4,11 +4,11 @@ set -e
 # Check if the SEEKLIT_CONF_FILE exists
 if [ ! -f "$SEEKLIT_CONF_FILE" ]; then
     echo "Configuration file not found at $SEEKLIT_CONF_FILE. Copying default.conf to $SEEKLIT_CONF_FILE..."
-    mkdir -p "$(dirname "$SEEKLIT_CONF_FILE")"  # Ensure the directory exists
+    mkdir -p "$(dirname "$SEEKLIT_CONF_FILE")"  # Create directory if it doesn't exist
     cp /app/default.conf "$SEEKLIT_CONF_FILE"
 else
     echo "Configuration file found at $SEEKLIT_CONF_FILE."
 fi
 
-# Execute the main process (your Go API binary)
+# Execute the main process
 /app/main
