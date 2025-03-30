1d186e53a8a3421e8a3469ed6454ff6ab3f67231
Austin Spencer <abspencer2097@gmail.com>
2025-03-30 11:08:02 -0400

    update comment

--- entrypoint-server.sh
+++ entrypoint-server.sh
@@ -4,7 +4,7 @@ set -e
 # Check if the SEEKLIT_CONF_FILE exists
 if [ ! -f "$SEEKLIT_CONF_FILE" ]; then
     echo "Configuration file not found at $SEEKLIT_CONF_FILE. Copying default.conf to $SEEKLIT_CONF_FILE..."
-    mkdir -p "$(dirname "$SEEKLIT_CONF_FILE")"  # Ensure the directory exists
+    mkdir -p "$(dirname "$SEEKLIT_CONF_FILE")"  # Create dir if it doesn't exist
     cp /app/default.conf "$SEEKLIT_CONF_FILE"
 else
     echo "Configuration file found at $SEEKLIT_CONF_FILE."
